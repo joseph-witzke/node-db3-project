0138d98f926f50e22844db4a9167238d2953f2c0
Joe Witzke <joewitzke@joes-mbp.lan>
2021-06-09 14:53:51 -0600

    Initial commit with checkSchemeId middleware written

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,5 @@
+const Scheme = require('../schemes/scheme-model');
+
 /*
   If `scheme_id` does not exist in the database:
 
@@ -7,8 +9,20 @@
   }
 */
 const checkSchemeId = (req, res, next) => {
-
-}
+  const { id } = req.params;
+  Scheme.findById(id)
+    .then((scheme) => {
+      if (!scheme) {
+        res
+          .status(404)
+          .json({ message: `scheme with scheme_id ${id} not found` });
+      } else {
+        req.scheme = scheme;
+        next();
+      }
+    })
+    .catch(next);
+};
 
 /*
   If `scheme_name` is missing, empty string or not a string:
@@ -18,9 +32,7 @@ const checkSchemeId = (req, res, next) => {
     "message": "invalid scheme_name"
   }
 */
-const validateScheme = (req, res, next) => {
-
-}
+const validateScheme = (req, res, next) => {};
 
 /*
   If `instructions` is missing, empty string or not a string, or
@@ -31,12 +43,10 @@ const validateScheme = (req, res, next) => {
     "message": "invalid step"
   }
 */
-const validateStep = (req, res, next) => {
-
-}
+const validateStep = (req, res, next) => {};
 
 module.exports = {
   checkSchemeId,
   validateScheme,
   validateStep,
-}
+};
